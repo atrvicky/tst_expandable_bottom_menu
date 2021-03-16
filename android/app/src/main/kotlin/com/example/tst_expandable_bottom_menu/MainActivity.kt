34961fed98b61e45770092f1461e35156637b90c
Joris Bekkers <jors@unravelsports.com>
2021-03-16 09:09:31 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/tst_expandable_bottom_menu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tst_expandable_bottom_menu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:tst_expandable_bottom_menu/unicorndial_2.dart';
+// import 'package:unicorndial/unicorndial.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: UnicornDialer(
+        parentButtonBackground: Colors.grey[700],
+        orientation: UnicornOrientation.HORIZONTAL,
+        parentButton: Icon(Icons.person),
+        childButtons: _getProfileMenu(),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+List<UnicornButton> _getProfileMenu() {
+  List<UnicornButton> children = [
+    UnicornButton(
+      currentButton: FloatingActionButton(
+        backgroundColor: Colors.grey[500],
+        mini: true,
+        child: Icon(Icons.account_balance),
+        onPressed: () {},
+      ),
+    ),
+    UnicornButton(
+      currentButton: FloatingActionButton(
+        backgroundColor: Colors.grey[500],
+        mini: true,
+        child: Icon(Icons.settings),
+        onPressed: () {},
+      ),
+    ),
+    // TODO
+    // UnicornButton(
+    //   currentButton: UnicornDialer(
+    //     parentButtonBackground: Colors.grey[700],
+    //     orientation: UnicornOrientation.VERTICAL,
+    //     parentButton: Icon(Icons.person),
+    //     childButtons: _getProfileMenu(),
+    //   ),
+    // ),
+  ];
+
+  return children;
+}
